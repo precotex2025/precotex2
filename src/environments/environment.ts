4a1f94c36886bbd02c184cf427234b1d48e64fd9
AhmedCodePrecotex <jbustamante@precotexperu.com>
2025-12-05 09:37:58 -0500

    Actualizacion urd backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,9 @@ export const environment = {
   //url: "http://localhost:5172/api/"
 
   //Produccion
+  url: "https://gestion.precotex.com:444/activosfijos/api/",
   //url: "http://activosfijos.precotex.com:7080/activosfijos/api/",
-  url: "http://192.168.1.36:7080/activosfijos/api/",
+  //- - ->>url: "http://192.168.1.36:7080/activosfijos/api/",
   url_UbiAlm: "https://gestion.precotex.com:444/ubicaciones/api/",
   //url_UbiAlm: "http://192.168.1.36:8062/api/",
 
